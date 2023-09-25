3e66ed89d444d8c0120ddda14693b554bca7b351
Dostonbek Yoqubov <dostonbek.yoqubov9830@gmail.com>
2023-09-25 23:36:35 +0500

    Add js file

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
-// var elUserForm = document.getElementById(text);
-// var elUserForm = document.getElementsByClassName(text);
-// var elUserForm = document.getElementsByTagName(p);
-
-//CREATING ELEMENTS AND STYLING 
-// let elBtn = document.createElement("button"); VARIABLE CREATED NAMED elBtn 
-// elBtn.textContent = "Send"; INSIDE OF THE elBtn PLACED TEXT 'Send'
-// document.body.prepend("button") ADDES ELEMENT AT THE TOP OF THE BODY
-// document.body.appendChild(elBtn); ELEMENT CREATES IN HTML BODY AS A CHILD 
-// document.body.style.backgroundColor = "orange"; - CHANGE BACKGROUND COLOR OF THE BODY
+let elUserForm = document.querySelector(".user-form-js");
+let elUserInput = document.querySelector(".user-input-js");
+let elUserSelect = document.querySelector(".user-selection-js")
+let elUserResult = document.querySelector(".user-output-js");
 
 
+elUserForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    let inputValue = +elUserInput.value;
+    let inputSelect = +elUserSelect.value;
+    let totalSum = inputValue / inputSelect;
 
-// let elUserForm = document.querySelector(".js-user-form");
-// let elUserInput = document.querySelector(".js-user-input");
-// let elUserFormBtn = document.querySelector(".js-form-btn");
-// let elUserCaption = document.querySelector(".js-form-caption");
+    if(inputSelect == 12200) {
+        elUserResult.textContent = "$" + totalSum.toFixed(2)
+    } else if (inputSelect == 13000) {
+        elUserResult.textContent = "€" + totalSum.toFixed(2)
+    } else if (inputSelect == 127) {
+        elUserResult.textContent = "₽" + totalSum.toFixed(2)
+    }
+});
 
-// console.log(elUserForm, elUserFormBtn, elUserInput);
-
-// elUserForm.addEventListener("submit", function(occur) {
-//     occur.preventDefault();
-//     var inputValue = elUserInput.value;
-//     elUserCaption.textContent = inputValue;
-//     console.log("Sent")
-// })
-
-var elUserForm = document.querySelector(".user-form-js");
-var elUserInput = document.querySelector(".user-input.js");
-// var elUserOption = document.querySelector(".user-selection-js");
-var elUserBtn = document.querySelector(".user-btn-js");
-var elUserResult = document.querySelector(".user-output-js");
-
-// console.log(elUserForm, elUserInput, elUserOption, elUserBtn, elUserResult);
-
-// var USD_CURRENCY = 11595;
-// var EUR_CURRENCY = 12849;
-// var RUB_CURRENCY = 128;
-
-elUserForm.addEventListener("submit", function(occur){
-    occur.preventDefault();
-    let inputValue = elUserInput.value;
-    elUserResult.textContent = inputValue;
-    
-})
 
